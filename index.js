1b600979d3bcb414f4670fb1a81d0d5dba71747e
aburk3 <aburk3@gmail.com>
2019-04-06 18:08:14 -0500

    Done.

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const user = 'aburk3';
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
+  return '';
   return 'fcfc03d73a1d5db9fb7efe652aab819ccae2e234';
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
+  //use fetch to fork it!
   const url = `${baseURL}/repos/${repo}/forks`;
   fetch(url, {
     method: 'POST',
@@ -21,12 +23,14 @@ function forkRepo() {
 }
 
 function showResults(json) {
+  //use this function to display the results from forking via the API
   document.getElementById('results').innerHTML = `<a href=${json.html_url}>${
     json.html_url
   }</a>`;
 }
 
 function createIssue() {
+  //use this function to create an issue based on the values input in index.html
   const repo = `${user}/js-ajax-fetch-lab`;
   const url = `${baseURL}/repos/${repo}/issues`;
   const postData = {
@@ -46,6 +50,7 @@ function createIssue() {
 }
 
 function getIssues() {
+  //once an issue is submitted, fetch all open issues to see the issues you are creating
   const repo = `${user}/js-ajax-fetch-lab`;
   const url = `${baseURL}/repos/${repo}/issues`;
   fetch(url, {
